e930a60c7d840f4cf2c5b2956122209063a2c812
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-10-01 12:09:43 -0400

    Updated from Brython Server: 10/1/2018 12:09:41 PM

--- cryptography.py
+++ cryptography.py
@@ -10,3 +10,23 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+text = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+if text != "e" and text != "d" and text != "q":
+    print("Did not understand command, try again.")
+    text = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+if text == "e":
+    message = input("Message: ")
+    key = input("Key: ")
+
+text1 =
+
+if text == "d":
+    print(message)
+
+
+
+scramble = associations.find(message)
+print(scramble)
