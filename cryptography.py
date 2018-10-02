dbcc0222c8950cd4daf68e93cc0ac7ea35d848bf
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-10-02 12:00:24 -0400

    Updated from Brython Server: 10/2/2018 12:00:23 PM

--- cryptography.py
+++ cryptography.py
@@ -12,21 +12,21 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
 text = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+def repeat_this(text):
+    print(text)
 
 if text != "e" and text != "q":
     print("Did not understand command, try again.")
-    text = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
 
 if text == "e":
     message = input("Message: ")
     key = input("Key: ")
-
-text1 = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-
-if text == "d":
-    print(message)
+def repeat_this(tex):
+    print(text)
 
 
 
-scramble = associations.find(message)
-print(scramble)
+
+#scramble = associations.find(message)
+#print(scramble)
