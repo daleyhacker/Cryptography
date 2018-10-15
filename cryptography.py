bc2f871d33a5abe6f6d38f5bac0b4e9833c9b16a
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-10-15 17:16:30 -0400

    Updated from Brython Server: 10/15/2018 5:16:30 PM

--- cryptography.py
+++ cryptography.py
@@ -23,12 +23,11 @@ if text == "e":
     
     for m in message:
         ma = associations.find(m)
-        while message == " ":
-            for k in key:
-                ka = associations.find(k)
-                encrypt = (ma)+(ka)
-                #print(encrypt)
-                print(associations[encrypt], end="")
+        for k in key:
+            ka = associations.find(k)
+            encrypt = (ma)+(ka)
+            #print(encrypt)
+            print(associations[encrypt], end="")
         
         """
         for k in key:
